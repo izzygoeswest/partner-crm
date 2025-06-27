4183c2c8a7553e57e60b4a5041b80f67d39b2cd6
izzygoeswest <izzygoeswest@gmail.com>
2025-06-27 14:24:16 -0500

    Create StudentPage.jsx

---
+++ src/pages/StudentPage.jsx
@@ -0,0 +1,10 @@
+import AddStudentForm from "../components/AddStudentForm";
+
+export default function StudentPage() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-4">Add New Student</h2>
+      <AddStudentForm />
+    </div>
+  );
+}
