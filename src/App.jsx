8c0baf1ded3734c427a165f2f8737aa8bf999e20
izzygoeswest <izzygoeswest@gmail.com>
2025-06-27 19:31:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import StudentPage from "./pages/StudentPage";
+import StudentList from "./pages/StudentList";
 
 // Components
 import PrivateRoute from "./components/PrivateRoute";
@@ -47,7 +48,12 @@ export default function App() {
       <nav className="mb-6 flex justify-between items-center">
         <div className="space-x-4">
           <Link to="/" className="font-semibold text-blue-600">Home</Link>
-          {user && <Link to="/students/new">Add Student</Link>}
+          {user && (
+            <>
+              <Link to="/students/new">Add Student</Link>
+              <Link to="/students" className="ml-4">View Students</Link>
+            </>
+          )}
         </div>
 
         <div className="space-x-4">
@@ -80,6 +86,14 @@ export default function App() {
           element={
             <PrivateRoute>
               <StudentPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/students"
+          element={
+            <PrivateRoute>
+              <StudentList />
             </PrivateRoute>
           }
         />
